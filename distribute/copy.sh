d9a8acd7b144d07bf2f8dd56b797cec5ecf287ca
Dai Yamazaki <yamadai@rainbow.iis.u-tokyo.ac.jp>
2021-04-09 18:10:47 +0900

    tar cvf bugfix
    bug fix

--- distribute/copy.sh
+++ distribute/copy.sh
@@ -4,14 +4,16 @@ TAG="v2.0_2021Feb"
 
 INPPBF="../../calc_2021Feb/extract_water/osm/water/planet-all.osm.pbf"
 INPTIF="../../calc_2021Feb/merge_water/tif"
+INPBIN="../../calc_2021Feb/merge_water/5deg"
 
 mkdir -p $TAG
-cd $TAG
+cd       $TAG
 
 cp    $INPPBF ./OSM_WaterLayer.pbf
 cp -r $INPTIF ./OSM_WaterLayer_tif
-tar -cvfz OSM_WaterLayer_tif.tar.gz OSM_WaterLayer_tif
+cp -r $INPBIN ./5deg
 
-###
+tar cvfz OSM_WaterLayer_tif.tar.gz OSM_WaterLayer_tif
+tar cvfz 5deg.tar.gz               5deg
 
 
--- distribute/copy_hires.sh
+++ distribute/copy_hires.sh
@@ -9,8 +9,8 @@ cd $TAG
 cp -r $INPBIN/1deg_10m   .
 cp -r $INPBIN/1deg_30m   .
 
-tar -cvfz 1deg_10m.tar.gz 1deg_10m
-tar -cvfz 1deg_30m.tar.gz 1deg_30m
+tar cvfz 1deg_10m.tar.gz 1deg_10m
+tar cvfz 1deg_30m.tar.gz 1deg_30m
 
 
 ###
